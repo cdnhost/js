6e64c0206fbb4bb4dc31ad1b5f3c9e005d28935f
cdnhost <redeblogx@gmail.com>
2017-06-05 16:24:33 -0300

    Create colormag-slider-setting.js

---
+++ colormag-slider-setting.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function(){
+   jQuery('.widget_slider_area_rotate').bxSlider({
+      mode: 'horizontal',
+      speed: 1500,
+      auto: true,
+      pause: 5000,
+      adaptiveHeight: true,
+      nextText: '',
+      prevText: '',
+      nextSelector: '.slide-next',
+      prevSelector: '.slide-prev',
+      pager: false,
+      tickerHover: true
+   });
+});
